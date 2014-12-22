2f37fac9c5e24e4b9c413e2ba6ee2a0fc26e013a
atolmachev <andrey.tolmachev@gmail.com>
2014-12-22 18:16:01 +0300

    Slight redesign, complete game

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,21 @@
 $(function () {
+    window.qn = 5;
+    initScore();
     nextQuestion();
 });
 
-var nextQuestion = function() {
+function initScore() {
+    $("#score").empty();
+    for (var i = 0; i < window.qn; i++) {
+        $("#score").append("<span class='score-glyph glyphicon glyphicon-star'></span>");
+    }
+    window.step = 0;
+    window.ok = 0;
+}
+
+function nextQuestion() {
     var poem = window.poemPool[randomInt(window.poemPool.length)];
-    $("#video").empty().append("<iframe width=420 height=315 class='embed-responsive-item' src='"+ poem.videoRef
+    $("#video").empty().append("<iframe width='420' height='315' class='embed-responsive-item' src='"+ poem.videoRef
     +"&showinfo=0&modestbranding=1&rel=0&fs=0&start=20' frameborder='0'></iframe>");
 
     $("#options").empty();
@@ -15,24 +26,84 @@ var nextQuestion = function() {
     $(".btn.no").click(function () {
         new PNotify({
             title: window.errMessages[randomInt(window.errMessages.length)],
-            text: 'Это же '+poem.options[poem.correctKey] +'!',
+            text: "Ведь это же <a href='"+ poem.poetLink +"' target='_blank'>" +poem.options[poem.correctKey] + "</a>&nbsp;!",
             type: 'error',
-            icon: 'glyphicon glyphicon-remove'
+            icon: 'glyphicon glyphicon-remove',
+            buttons: {
+                sticker: false,
+                closer: false
+            }
         });
+        initScore();
         nextQuestion();
     });
     $(".btn.yes").click(function () {
+        window.ok++;
         new PNotify({
             title: window.okMessages[randomInt(window.okMessages.length)],
             text: 'А стихи читал '+poem.actorName,
-            type: 'success'
+            type: 'success',
+            buttons: {
+                sticker: false,
+                closer: false
+            }
         });
-        nextQuestion();
+        $(".score-glyph").eq(window.step).addClass("score-success");
+        window.step++;
+        if (window.step == window.qn) {
+            show_stack_info();
+        } else {
+            nextQuestion();
+        }
     });
 
     $('.container').hide().show(0);
-};
+}
 
-var randomInt = function(n) {
+function randomInt(n) {
     return Math.floor(Math.random() * n)
-};
+}
+
+function show_stack_info() {
+    var modal_overlay;
+    if (typeof info_box != "undefined") {
+        info_box.open();
+        return;
+    }
+    info_box = new PNotify({
+        title: 'Поздравляем!',
+        text: "Вы отлично разбираетесь в русской поэзии! \n Вы можете \n &nbsp; <a href='index.html'>Сыграть еще раз</a> \n &nbsp; <a href='http:\\vk.com' target='_blank'>Рассказать друзьям</a>",
+        type: 'success',
+        icon: 'glyphicon glyphicon-flag',
+        hide: false,
+        stack: false,
+        width: "500px",
+        buttons: {
+            closer: false,
+            sticker: false
+        },
+        before_open: function(PNotify) {
+            // Position this notice in the center of the screen.
+            PNotify.get().css({
+                "top": ($(window).height() / 2) - (PNotify.get().height() / 2),
+                "left": ($(window).width() / 2) - (PNotify.get().width() / 2)
+            });
+            // Make a modal screen overlay.
+            if (modal_overlay) modal_overlay.fadeIn("fast");
+            else modal_overlay = $("<div />", {
+                "class": "ui-widget-overlay",
+                "css": {
+                    "display": "none",
+                    "position": "fixed",
+                    "top": "0",
+                    "bottom": "0",
+                    "right": "0",
+                    "left": "0"
+                }
+            }).appendTo("body").fadeIn("fast");
+        },
+        before_close: function() {
+            modal_overlay.fadeOut("fast");
+        }
+    });
+}
--- js/data.js
+++ js/data.js
@@ -3,41 +3,48 @@ window.poemPool = [
         videoRef: "https://www.youtube.com/embed/PC5Q1qIHySU?start=60",
         options: ["Есенин", "Блок", "Пушкин", "Бальмонт"],
         correctKey: 2,
+        poetLink: "http://goo.gl/OiNFYi",
         actorName: "Иннокентий Смоктуновский"
     },
     {
         videoRef: "https://www.youtube.com/embed/rwanD-euW6Q?start=1",
         options: ["Пушкин", "Лермонтов", "Достоевский", "Некрасов"],
         correctKey: 0,
+        poetLink: "http://goo.gl/OiNFYi",
         actorName: "Андрей Миронов"
     },
     {
         videoRef: "https://www.youtube.com/embed/mvJz7bE1eLc?start=25",
         options: ["Пастернак", "Церетели", "Нонешвили", "Бараташвили"],
         correctKey: 3,
+        poetLink: "http://goo.gl/d6x19o",
         actorName: "Резо Габриадзе"
     },
     {
         videoRef: "https://www.youtube.com/embed/pJzZbuFny04?start=9",
         options: ["Пастернак", "Высоцкий", "Ахматова", "Есенин"],
         correctKey: 0,
+        poetLink: "http://goo.gl/rvKOIJ",
         actorName: "Владимир Высоцкий"
     },
     {
         videoRef: "https://www.youtube.com/embed/o9Hm9yoMLdU?start=3",
         options: ["Миронов", "Есенин", "Кривошеев", "Рождественский"],
         correctKey: 2,
+        poetLink: "http://goo.gl/8MNu23",
         actorName: "Евгений Миронов"
     },
     {
         videoRef: "https://www.youtube.com/embed/G0aH3WaK2o8?start=23",
         options: ["Гумилев", "Мандельштам", "Блок", "Пастернак"],
+        poetLink: "http://goo.gl/PhyOyE",
         correctKey: 0,
         actorName: "Кирилл Пирогов"
     },
     {
         videoRef: "https://www.youtube.com/embed/KIkCtDRGN1c?start=5&end=61",
         options: ["Цветаева", "Ахмадулина", "Ахматова", "Берггольц"],
+        poetLink: "http://goo.gl/0lUqQt",
         correctKey: 1,
         actorName: "Аня Чиповская"
     },
@@ -45,18 +52,21 @@ window.poemPool = [
         videoRef: "https://www.youtube.com/embed/jY5CkSzcwMc?start=6&end=39",
         options: ["Симонов", "Берггольц", "Шпаликов", "Тарковский"],
         correctKey: 2,
+        poetLink: "http://goo.gl/TcXMEm",
         actorName: "Александр Яценко"
     },
     {
         videoRef: "https://www.youtube.com/embed/h6kUlSvpuiw?start=5&end=50",
         options: ["Шпаликов", "Симонов", "Твардовкий", "Друнина"],
         correctKey: 0,
+        poetLink: "http://goo.gl/TcXMEm",
         actorName: "Михаил Ефремов"
     },
     {
         videoRef: "https://www.youtube.com/embed/Nm6fRotHrnk?start=13&end=113",
-        options: ["Бродский"],
-        correctKey: 0,
+        options: ["Пастернак", "Бродский", "Лермонтов", "Ахмадулина"],
+        correctKey: 1,
+        poetLink: "http://goo.gl/OMDW2B",
         actorName: "Сергей Юрский"
     }
 ];
